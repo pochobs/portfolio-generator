0ba84b01a350302c39ab993c9856355436f40dfc
Pochobs <pochobs@hotmail.com>
2020-07-15 02:32:47 -0500

    Module 9.2.6 Added HTML template

--- app.js
+++ app.js
@@ -1,19 +1,30 @@
 
-const profileDataArgs = process.argv.slice(2, process.argv.length);
-console.log(profileDataArgs)
+// const profileDataArgs = process.argv.slice(2, process.argv.length);
+// console.log(profileDataArgs)
 
-// console.log(profileDataArgs);// Notice the lack of parentheses around the `profileDataArr` parameter?
+// // console.log(profileDataArgs);// Notice the lack of parentheses around the `profileDataArr` parameter?
 
-const printProfileData = profileDataArr => {
-    for (let i = 0; i < profileDataArr.length; i += 1) {
-      console.log(profileDataArr[i]);
+// const printProfileData = profileDataArr => {
+//     for (let i = 0; i < profileDataArr.length; i += 1) {
+//       console.log(profileDataArr[i]);
       
-    }
-    console.log('================');
-    profileDataArr.forEach(profileItem => console.log(profileItem));
+//     }
+//     console.log('================');
+//     profileDataArr.forEach(profileItem => console.log(profileItem));
       
     
-};
+// };
   
-printProfileData(profileDataArgs);
+// printProfileData(profileDataArgs);
+const fs = require('fs');
+const generatePage = require('./src/page-template');
 
+const profileDataArgs = process.argv.slice(2);
+
+const [name, github] = profileDataArgs;
+
+fs.writeFile('index.html', generatePage(name, github), err => {
+  if (err) throw err;
+
+  console.log('Portfolio complete! Check out index.html to see the output!');
+});
