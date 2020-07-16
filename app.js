f21d57eb32bde3c1c38ad3b74b69f5db80626f15
Pochobs <pochobs@hotmail.com>
2020-07-15 23:43:40 -0500

    Module 9.3.5 - First prompt question - What is your name?

--- app.js
+++ app.js
@@ -18,13 +18,24 @@
 // printProfileData(profileDataArgs);
 // const fs = require('fs');
 // const generatePage = require('./src/page-template');
-const inquirer = require('inquirer');
-const profileDataArgs = process.argv.slice(2);
 
-const [name, github] = profileDataArgs;
+// const profileDataArgs = process.argv.slice(2);
+
+// const [name, github] = profileDataArgs;
 
 // fs.writeFile('index.html', generatePage(name, github), err => {
   // if (err) throw err;
 
   // console.log('Portfolio complete! Check out index.html to see the output!');
 // });
+
+const inquirer = require('inquirer');
+inquirer
+  .prompt([
+    {
+      type: 'input',
+      name: 'name',
+      message: 'What is your name?'
+    }
+  ])
+  .then(answers => console.log(answers));
